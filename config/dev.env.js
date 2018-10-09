fffec03d845d4a6026b0f81f4da703d619692dd7
Leonardo Vaquel <leonardovaquel@gmail.com>
2018-10-09 20:42:38 -0300

    Se cambio el directorio root de la app

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,5 +5,5 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   DEBUG_MODE: true,
-  API_BASE: '"http://localhost:8000"'
+  API_BASE: '"http://localhost:3000"'
 })
