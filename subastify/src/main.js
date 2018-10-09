9cc915e5cfbea7f4d0550bd1344251b2bf10da2a
Leonardo Vaquel <leonardovaquel@gmail.com>
2018-10-09 20:22:31 -0300

    Se importo el store y el vuex router sync

--- subastify/src/main.js
+++ subastify/src/main.js
@@ -5,8 +5,12 @@ import App from './App'
 import router from './router'
 import Vuetify from 'vuetify'
 
+import { sync } from 'vuex-router-sync'
+import store from './store'
+
 Vue.use(Vuetify)
 
+sync(store, router)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
